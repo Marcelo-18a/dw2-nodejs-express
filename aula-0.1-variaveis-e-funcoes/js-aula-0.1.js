a846f46b927c98a2bc569c4e39b7861808dd21d9
Marcelo-18a <marceloyogo37@gmail.com>
2025-02-13 18:46:09 -0300

    Variável e funções no JS

---
+++ aula-0.1-variaveis-e-funcoes/js-aula-0.1.js
@@ -0,0 +1,14 @@
+//comentario em js
+/*Comentario em bloco... */
+// declaração de variaveis no js 
+// COSNT X LET X VAR
+const nome = "Marcelo"
+let Nome
+//const nome = "pedro" -> resulta em erro pq ja tem uma const
+//let Nome -> resulata em erro por ja ter uma variavel em let com o nome "Nome"
+var cor = "azul"
+var cor = "amarelo"
+let cidade = "pariquera-açu"
+const message = "hello world!! Iniciando estudos com java script!"
+
+console.log(nome, message , cidade , cor)
