543420d12680197cfce78349940c3dccf7497fdf
Marcelo-18a <marceloyogo37@gmail.com>
2025-03-12 22:02:21 -0300

    Vida boa as reis

--- aula0.2-arrays-e-objetos/js-aula-0.2-arrays-e-objetos.js
+++ aula0.2-arrays-e-objetos/js-aula-0.2-arrays-e-objetos.js
@@ -24,3 +24,51 @@ console.log("exibindo a lista através do forEach com os indices...")
 Produtos.forEach((Produtos,i)=>{
     console.log(`${i+1} - ${Produtos}`)
 })
+
+// metodso de manipulaçãio de vetores
+
+let vetor = ['Laranja','maça','banana']
+console.log(`Nosso vetor é o: ${vetor}`)
+vetor[3]= 'Morango'
+console.log(`Nosso vetor agora é o: ${vetor}`)
+
+//metodo push - insere um novo elemento no final do vetor
+
+vetor.push('abacaxi')
+console.log(`Nosso vetor é o: ${vetor}`)
+
+// METODO UNSHIFT - insere novo elemento no inicio do vetor
+
+vetor[0] = 'pera'
+console.log(`Nosso vetor é o: ${vetor}`)
+vetor.unshift('Laranja')
+console.log(`Nosso vetor é o: ${vetor}`)
+
+// METODO LENGTH - retorna o numero de elementos no vetor
+
+let numeros = [6,8,2,9,3]
+console.log(`Nossa lista de numeros é: ${numeros}`)
+console.log(`O NUMERO DE ELEMENTOS DO VETOR É ${numeros.length}`)
+
+//METODO SORT - ordernar o vetor
+
+console.log(`o primeiro element da lista de frutas é: ${vetor}.`)
+vetor.sort()
+console.log(`o primeiro element da lista de frutas é: ${vetor}.`)
+
+//ordanação de numeros com sort
+
+console.log(`Nossa lista de numeros é ${numeros}`)
+numeros.sort()
+console.log(`Nossa lista de numeros é ${numeros}`)
+
+// ordenando de forma crescente
+
+console.log(numeros.sort((a,b) => b - a))
+
+//////////////////////////////////////////////////////
+//objetos literais (nao são derivados de classes)
+//objetos possuem atributos (caracteristicas) e metodos (ações) 
+//lado esquerdo: chaves/ lado direito: valores
+const pessoa = {}
+console.log(typeof(pessoa))
